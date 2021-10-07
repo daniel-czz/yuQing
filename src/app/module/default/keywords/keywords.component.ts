ba09be151392d56044f71596cba93a6fe0660b08
daniel chen <daniel@bluemythglobal.com>
2021-10-07 14:37:35 -0700

    update keywords add/modify/delete

--- src/app/module/default/keywords/keywords.component.ts
+++ src/app/module/default/keywords/keywords.component.ts
@@ -17,58 +17,135 @@ export class KeywordsComponent implements OnInit {
               }
 
   ngOnInit(): void {
-    this.listData = this.localstorage.get("listData");
-    if(!this.localstorage.get("listData")){
-      this.getKeyWords();
-    }
+    
+    this.getKeyWords();
+    
     
     
   }
 
   key = {
-  keyword: "",
-  may_keyword: "",
-  nokeyword: "",
-  frequency: "",
-};
+    keyword: "",
+    may_keyword: "",
+    nokeyword: "",
+    frequency: "",
+  };
+
+  editKey = {
+    id:"",
+    keyword: "",
+    may_keyword: "",
+    nokeyword: "",
+    frequency: "",
+  };
 
 listData: any= {};
 
 userInfo: any= {};
   
-  isVisible = false;
-  isOkLoading = false;
+isAddVisible = false; //增加关键词
+isModifyVisible = false; //编辑关键词
+isOkLoading = false;
 
-  showModal(): void {
-    this.isVisible = true;
+
+
+
+  showAddModal(): void {
+    this.isAddVisible = true;
+  }
+
+
+  showModifyModal(dataId: any): void{
+    this.isModifyVisible = true;
+    var api = "http://yuqing.itying.com/api/oneKeywordsList?id="+ dataId; 
+    this.httpService.get(api, {
+      auth: {
+        username: this.userInfo.token,
+        password:"",
+      }
+    }).then((response: any)=>{
+        // console.log("1111111");
+        this.editKey = response.data.result;
+        console.log(response);
+    })
+  }
+
+   handleCancel(): void {
+    this.isAddVisible = false;
+    this.isModifyVisible = false;
   }
 
   addKeyWords(): void {
+    
+    console.log(this.key);
     this.isOkLoading = true;
-    setTimeout(() => {
-      console.log(this.key);
-      this.listData.push(this.key);
-      this.localstorage.set("listData", this.listData);
-      this.key = { //把key重新清空
+    var api= "http://yuqing.itying.com/api/addKeywords";
+    this.httpService.post(api, this.key, {
+      auth: {
+        username: this.userInfo.token,
+        password:"",
+      }
+    }).then((response: any)=>{
+        console.log(response);
+        if(response.data.success == false){
+          alert(response.data.message);
+        }
+        this.getKeyWords();
+    })
+    this.isAddVisible = false;
+    this.isOkLoading = false;
+    this.key = { //把key重新清空
         keyword: "",
         may_keyword: "",
         nokeyword: "",
         frequency: "",
       }
-      this.isVisible = false;
-      this.isOkLoading = false;
-    }, 1000);
   }
 
-  handleCancel(): void {
-    this.isVisible = false;
+
+  deleteKeyword(dataId: any){
+
+    var flag = confirm("Do you comfirm to delete?")
+    if(flag){
+      var api= "http://yuqing.itying.com/api/deleteKeywords?id=" + dataId;
+    this.httpService.get(api, {
+      auth: {
+        username: this.userInfo.token,
+        password:"",
+      }
+    }).then((response: any)=>{
+        console.log(response);
+        this.getKeyWords();
+    })
+    }
+    
   }
 
-  delete(index: any){
-    //console.log("delete index # " + index + " "+ "item in list");
-    this.listData.splice(index,1);
-    this.localstorage.set('listData',this.listData);
-    //console.log(this.localstorage.get("listData"));
+  //执行修改
+  modifyKeyWords(){
+    console.log(this.key);
+    this.isOkLoading = true;
+    var api= "http://yuqing.itying.com/api/editKEywords";
+    this.httpService.post(api, this.editKey, {
+      auth: {
+        username: this.userInfo.token,
+        password:"",
+      }
+    }).then((response: any)=>{
+        console.log(response);
+        if(response.data.success == false){
+          alert(response.data.message);
+        }
+        this.getKeyWords();
+    })
+    this.isModifyVisible = false;
+    this.isOkLoading = false;
+    this.key = { //把key重新清空
+        keyword: "",
+        may_keyword: "",
+        nokeyword: "",
+        frequency: "",
+      }
   }
 
   //获取舆情关键词
@@ -83,7 +160,7 @@ userInfo: any= {};
         console.log(response);
         
         if(response.data.success == true){
-                   this.listData = response.data.result;
+            this.listData = response.data.result;
          }
     })
   }
